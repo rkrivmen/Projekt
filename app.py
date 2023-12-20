a3078b58d03405970255655f4d0b048028c711f9
libakoer <arturtamm02@gmail.com>
2023-12-20 02:15:43 +0200

    kõik on maksimumis

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ def home():
         nimi[i[0]]=i[1]
     f.close()
     error = None
-    if request.method == 'POST':
+    if request.method == 'POST' and 'registreeri' in request.form:
+        return redirect(url_for("registreeri"))
+    elif request.method == 'POST':
         a= request.form['username']
         b=request.form['password']
         if a in nimi and b ==nimi[a] :
@@ -21,17 +23,40 @@ def home():
         else:
             error = 'Invalid Credentials. Please try again.'
     return render_template('log.html', error=error)
+@app.route('/registreeri', methods=['GET', 'POST'])
+def registreeri():
+    error = None
+    if request.method == 'POST' and 'tagasi' in request.form:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        f=open("paroolid.txt","r",encoding="utf-8")
+        nimi={}
+        g=[]
+        c=0
+        for i in f:
+            g.append(i)
+            i=i.strip("\n").split(";")
+            nimi[i[0]]=i[1]
+        f.close()
+        a= request.form['usernamep']
+        b=request.form['passwordp']
+        if a in nimi:
+            error = 'Nimi on juba võetud'
+        else:
+            uus_nimi=[a,b]
+            c=1
+            error="Registreeritud"
+        f=open("paroolid.txt","w",encoding="utf-8")
+        for i in g:
+            f.write(i)
+            print(i)
+        if c==1:
+            f.write("\n"+uus_nimi[0]+";"+uus_nimi[1])
+        f.close()
+    return render_template('reg.html', error=error)
 
 @app.route("/tabel")
 def tabel():
-    f=open("paevad.txt","r",encoding="utf-8")
-    ajad={}
-    for i in f:
-        i=i.strip("\n").split(";")
-        ajad[i[0]]=i[1]
-    if session["user"] in ajad:
-        aeg=ajad[session["user"]].split(":")
-    print(session["user"])
     return render_template("main.html")
 @app.route('/send-date', methods=['POST'])
 def send_date():
@@ -61,19 +86,22 @@ def get_highlighted_days(year, month):
         else:
             ajad[i[0]]=[i[1::]]
     f.close()
-    aeg=ajad["artur"]
-    for i in aeg:
-        if i[0] in highlighted_days:
-            osa=highlighted_days[str(i[0])]
-            osa.append([str(i[1]),int(i[2])])
-            highlighted_days[str(i[0])]=osa
-        else:
-            highlighted_days[str(i[0])]=[[str(i[1]),int(i[2])]]
-    if year in highlighted_days:
-        highlighted_for_year = highlighted_days[year]
-        
-        highlighted_for_month = [day[1] for day in highlighted_for_year if day[0] == month]
-        return jsonify(highlighted_for_month)
+    try:    
+        aeg=ajad[session["user"]]
+        for i in aeg:
+            if i[0] in highlighted_days:
+                osa=highlighted_days[str(i[0])]
+                osa.append([str(i[1]),int(i[2])])
+                highlighted_days[str(i[0])]=osa
+            else:
+                highlighted_days[str(i[0])]=[[str(i[1]),int(i[2])]]
+        if year in highlighted_days:
+            highlighted_for_year = highlighted_days[year]
+            
+            highlighted_for_month = [day[1] for day in highlighted_for_year if day[0] == month]
+            return jsonify(highlighted_for_month)
+    except:
+        pass
     
     return jsonify([])
 
@@ -115,7 +143,6 @@ def paev():
         if c!=1:
             f.write("\n"+session["user"]+":"+str(session["year"])+":"+str(session["month"])+":"+str(session["day"])+":"+"<br>"+a+"<br>")
 
-        file=open("paevad.txt","r",encoding="utf-8")
         f.close()
 
     f=open("tegevus.txt","r",encoding="utf-8")
